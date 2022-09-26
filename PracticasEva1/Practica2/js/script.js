7c0164fdb75ef3d2d3ee62a73911e83454293a94
Pedro <9827@cifpceuta.es>
2022-09-26 08:53:43 +0200

    first

---
+++ PracticasEva1/Practica2/js/script.js
@@ -0,0 +1,21 @@
+let alto = 0;
+do {
+    alto = prompt("Introduzca el alto de la tabla:");
+
+} while (isNaN(alto));
+
+let ancho = 0;
+do {
+    ancho = prompt("Introduzca el ancho de la tabla:");
+
+} while (isNaN(ancho));
+
+document.write("<table>");
+for (let i = 1; i <= alto; i++) {
+    document.write("<tr>");
+    for (let j = 1; j <= ancho; j++) {
+        document.write("<td>" + i + "-" + j + "</td>");
+    }
+    document.write("<tr>");
+}
+document.write("</table>");
