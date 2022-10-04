5ac4bc3f79f02f2e20cd909cc9fd1d1f2612ec1f
Pedro <9827@cifpceuta.es>
2022-10-04 13:52:37 +0200

    04 10 22

--- PracticasEva1/Practica9/js/script.js
+++
@@ -1,7 +0,0 @@
-let passwd = prompt("Introduzca su contraseña");
-const CHARS = "aáàbcçdeéèfghiìíjklmnñoóòpqrstuùúüvwxyz0123456789";
-let isPasswd = false;
-
-for (let i = 0; i < CHARS.length; i++) {} //includes
-
-alert(isPasswd);
