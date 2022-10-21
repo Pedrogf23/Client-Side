5c9c385c4f6e01e4794f69a8e2e2a008d19b461d
Pedro <9827@cifpceuta.es>
2022-10-21 11:37:50 +0200

    21 10 22

---
+++ Ejercicios/T4/ejercicio1/scriptej1.js
@@ -0,0 +1,9 @@
+function parImpar(num) {
+    return num % 2 == 0 ? 'par' : 'impar';
+}
+function escribirNumsParImpar() {
+    for (let i = 0; i < 50; i++) {
+        let num = Math.random() * 10000 + 1;
+        document.write(num + ' es ' + parImpar(num) + '<br>');
+    }
+}
--- PracticasEva1/Practica13/js/funciones.js
+++ PracticasEva1/Practica13/js/funciones.js
@@ -1,29 +1,40 @@
 // Se le pide al usuario el número de filas y columnas para crear el tablero.
-let filas = parseInt(prompt('Introduce las filas del tablero.'));
-let columnas = parseInt(prompt('Introduce las columnas del tablero.'));
+let filasUser = parseInt(prompt('Introduce las filas del tablero.'));
+let columnasUser = parseInt(prompt('Introduce las columnas del tablero.'));
 // Si lo introducido no coincide con las condiciones válidas, se piden de nuevo.
-while ((filas < 2) | (columnas < 2) | (filas > 20) | (columnas > 20)) {
+while (
+    (filasUser < 2) |
+    (columnasUser < 2) |
+    (filasUser > 20) |
+    (columnasUser > 20)
+) {
     alert('Has introducido parámetros inválidos, intentalo de nuevo.');
-    filas = parseInt(prompt('Introduce las filas del tablero.'));
-    columnas = parseInt(prompt('Introduce las columnas del tablero.'));
+    filasUser = parseInt(prompt('Introduce las filas del tablero.'));
+    columnasUser = parseInt(prompt('Introduce las columnas del tablero.'));
 }
 
 // Funcion que, dadas las filas y las columnas, se le pasa un array y lo rellena de 0.
-function rellenarTablero(array, filas, columnas) {
-    if (isNaN(filas) | isNaN(columnas) | (typeof array != Object)) {
-        return false;
-    } else {
-        for (let i = 0; i < filas; i++) {
-            for (let j = 0; j < columnas; j++) {
-                tablero[i][j] = 0;
-            }
+function rellenarTablero(filas, columnas) {
+    tablero = [[]];
+    for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < columnas; j++) {
+            tablero[i][j] = 0;
         }
-        return tablero;
+    }
+    return tablero;
+}
+
+let tablero = rellenarTablero(filasUser, columnasUser);
+
+for (let i = 0; i < filasUser; i++) {
+    document.write('<br>');
+    for (let j = 0; j < columnasUser; j++) {
+        document.write(tablero[i][j]);
     }
 }
 
-let minas = parseInt(prompt('Introduce las minas del tablero.'));
-while ((minas < 1) | (minas > (filas * columnas) / 2)) {
-    alert('Has introducido parámetros inválidos, intentalo de nuevo.');
-    minas = parseInt(prompt('Introduce las minas del tablero.'));
-}
+// let minas = parseInt(prompt('Introduce las minas del tablero.'));
+// while ((minas < 1) | (minas > (filas * columnas) / 2)) {
+//     alert('Has introducido parámetros inválidos, intentalo de nuevo.');
+//     minas = parseInt(prompt('Introduce las minas del tablero.'));
+// }
