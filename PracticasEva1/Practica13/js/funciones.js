1e34c458346ac09bc06abed0a564abb894b9e9eb
Pedro <9827@cifpceuta.es>
2022-10-21 12:28:31 +0200

    21 10 22

--- PracticasEva1/Practica13/js/funciones.js
+++ PracticasEva1/Practica13/js/funciones.js
@@ -1,40 +1,10 @@
-// Se le pide al usuario el número de filas y columnas para crear el tablero.
-let filasUser = parseInt(prompt('Introduce las filas del tablero.'));
-let columnasUser = parseInt(prompt('Introduce las columnas del tablero.'));
-// Si lo introducido no coincide con las condiciones válidas, se piden de nuevo.
-while (
-    (filasUser < 2) |
-    (columnasUser < 2) |
-    (filasUser > 20) |
-    (columnasUser > 20)
-) {
-    alert('Has introducido parámetros inválidos, intentalo de nuevo.');
-    filasUser = parseInt(prompt('Introduce las filas del tablero.'));
-    columnasUser = parseInt(prompt('Introduce las columnas del tablero.'));
+'use strict';
+
+function pedirDatos(dato) {
+    dato = prompt('Introduce las ' + dato + '.');
+    return dato;
 }
 
-// Funcion que, dadas las filas y las columnas, se le pasa un array y lo rellena de 0.
-function rellenarTablero(filas, columnas) {
-    tablero = [[]];
-    for (let i = 0; i < filas; i++) {
-        for (let j = 0; j < columnas; j++) {
-            tablero[i][j] = 0;
-        }
-    }
-    return tablero;
-}
+let filas = parseInt(pedirDatos('filas'));
 
-let tablero = rellenarTablero(filasUser, columnasUser);
-
-for (let i = 0; i < filasUser; i++) {
-    document.write('<br>');
-    for (let j = 0; j < columnasUser; j++) {
-        document.write(tablero[i][j]);
-    }
-}
-
-// let minas = parseInt(prompt('Introduce las minas del tablero.'));
-// while ((minas < 1) | (minas > (filas * columnas) / 2)) {
-//     alert('Has introducido parámetros inválidos, intentalo de nuevo.');
-//     minas = parseInt(prompt('Introduce las minas del tablero.'));
-// }
+alert(filas);
