00b4c8ee8154a0191a95d7999ade4e0e2d701ffa
Pedro <9827@cifpceuta.es>
2022-11-08 17:47:37 +0100

    08 11 22

--- ProyectoEVA1/funciones.js
+++ ProyectoEVA1/funciones.js
@@ -48,21 +48,18 @@ function getN_minas(nFilas, nColumnas) {
 
 // Funcion que coloca las minas en el tablero de forma aleatoria.
 function colocaMinas(tablero, minas) {
+    // Variables que almacenarán coordenadas x e y aleatorias.
     let x = 0;
     let y = 0;
     for (let i = 0; i < minas; i++) {
+        // Se generarán coordenadas nuevas siempre que en las actuales ya haya una mina.
         do {
-            // Primero se generan unas coordenadas aleatorias dentro del tablero.
-            // Coordenadas en el eje x (fila).
-            x = parseInt(Math.random() * tablero.length);
-            // Coordenadas en el eje y (columna).
-            y = parseInt(Math.random() * tablero[0].length);
-            // Las coordenadas se van a generar siempre que se encuentre ya una mina en esa posicion.
-        } while ((tablero[x][y] = 'MINA'));
-        // Una vez se encuentren unas coordenadas donde no haya minas, se guarda una mina ahí.
+            x = parseInt(Math.random() * tablero.length); // tablero.length representa la cantidad de filas que contiene el array.
+            y = parseInt(Math.random() * tablero[0].length); // tablero[0].length representa la cantidad de columnas que contiene el array.
+        } while (tablero[x][y] == 'MINA');
+        // Cuando no haya mina, en esas coordenadas se colocará una.
         tablero[x][y] = 'MINA';
     }
-    // Esto se va a repetir tantas veces como minas se hayan pasado por parámetros.s
 }
 
 // Funcion que cuenta el número de minas que haya alrededor de una casilla.
@@ -72,15 +69,12 @@ function nMinasPos(tablero, fila, columna) {
     let numMinas = 0;
 
     // Se recorren las filas contiguas a la recibida por parámetro.
-    for (let zFila = fila - 1; zFila < fila + 1; zFila++) {
+    for (let zFila = fila - 1; zFila <= fila + 1; zFila++) {
         // Se recorren las columnas contiguas a la recibida por parámetro.
-        for (let zCol = columna - 1; zCol < columna + 1; zCol++) {
-            // Se comprueba que la casilla que estamos
-            // comprobando se encuentre dentro del tablero.
-            if (zFila > -1 && zCol > -1 && zFila < tablero.length && zCol < tablero[0].length) {
-                // Si está dentro, comprueba que en esa casilla haya una mina.
-                if (tablero[zFila][zCol] == 'MINA') {
-                    // Si la hay, aumenta el número de minas;
+        for (let zColumna = columna - 1; zColumna <= columna + 1; zColumna++) {
+            if (zFila > -1 && zColumna > -1 && zFila < tablero.length && zColumna < tablero[0].length) {
+                // Entonces, si en esa casilla hay una mina.
+                if (tablero[zFila][zColumna] == 'MINA') {
                     numMinas++;
                 }
             }
@@ -99,41 +93,53 @@ function colocaRelleno(tablero) {
             // Si la casilla actual NO tiene mina.
             if (tablero[fila][columna] != 'MINA') {
                 // Se guarda en dicha casilla el número de minas que tenga alrededor.
-                tablero[fila][columna] = nMinasPos(fila, columna);
+                tablero[fila][columna] = nMinasPos(tablero, fila, columna);
             }
         }
     }
 }
 
 // Funcion que dado el tablero ya relleno completamente, lo muestra por pantalla.
-function dibujaTablero(tableroArray) {
+function dibujaTablero(tablero) {
     // Recogemos el elemento del html que almacenará el tablero.
-    let tablero = document.getElementById('tablero');
+    let tableroDiv = document.getElementById('tablero');
     // Cambiamos el valor de las variables en css.
-    document.querySelector('html').style.setProperty('--num-filas', tableroArray.length);
-    document.querySelector('html').style.setProperty('--num-columnas', tableroArray[0].length);
+    document.querySelector('html').style.setProperty('--num-filas', tablero.length);
+    document.querySelector('html').style.setProperty('--num-columnas', tablero[0].length);
     // El tema del css está sacado de un ejemplo en internet, hecho para que el tablero
     // quede mejor visualmente en pantalla.
 
     // Se recorren las filas del tablero.
-    for (let fila = 0; fila < tableroArray.length; fila++) {
+    for (let fila = 0; fila < tablero.length; fila++) {
         // Dentro de cada fila, se recorren las columnas.
-        for (let columna = 0; columna < tableroArray[0].length; columna++) {
-            if (tableroArray[fila][columna] == 'MINA') {
-                // Si la casilla contiene una mina, imprime un div
-                // que contiene un emoticono de bomba.
-                tablero.innerHTML += `<div>\u{1F4A3}</div>`;
-            } else if (tableroArray[fila][columna] == 0) {
-                // Si la casilla contiene un 0, imprime un div vacío.
-                tablero.innerHTML += `<div></div>`;
+        for (let columna = 0; columna < tablero[0].length; columna++) {
+            // Si en la casilla hay una mina, dibuja un div con un emoticono dentro.
+            if (tablero[fila][columna] == 'MINA') {
+                tableroDiv.innerHTML += `<div>\u{1F4A3}</div>`;
+                // Si hay un 0, dibuja un div vacío.
+            } else if (tablero[fila][columna] == 0) {
+                tableroDiv.innerHTML += `<div></div>`;
+                // Y si no, dibuja un div con el contenido de la casilla correspondiente.
             } else {
-                // Si la casilla no contiene ni mina ni 0,
-                // imprime un div con el contenido correspondiente del tablero.
-                tablero.innerHTML += `<div>${tableroArray[f][c]}</div>`;
+                tableroDiv.innerHTML += `<div>${tablero[fila][columna]}</div>`;
             }
         }
     }
 }
 
 // Función que gestiona los datos iniciales y llama a las demás funciones.
-function buscaminas() {}
+function buscaminas() {
+    // Primero, se inicializa el tablero, usando las funciones para pedir filas y columnas como parámetros.
+    let tablero = iniciaTablero(getN_filas(), getN_columnas());
+
+    // Posteriormente, con el tablero ya inicializado, se colocan en él las minas,
+    // usando la funcion para pedir minas como parámetro.
+    colocaMinas(tablero, getN_minas(tablero.length, tablero[0].length));
+
+    // Una vez las tenemos el tablero con las minas, hay que rellenar
+    // las casillas con el número de minas que tenga alrededor.
+    colocaRelleno(tablero);
+
+    // Una vez ya tenemos el array totalmente relleno, lo mostramos por pantalla.
+    dibujaTablero(tablero);
+}
